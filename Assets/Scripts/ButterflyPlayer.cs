257216fc7caba0ca0d76259365d0c16bbba41109
Luke Alexander <23AlexL@live.elida.k12.oh.us>
2021-03-08 08:50:58 -0500

    Butterfly Death

--- Assets/Scripts/ButterflyPlayer.cs
+++ Assets/Scripts/ButterflyPlayer.cs
@@ -1,31 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButterflyPlayer : MonoBehaviour
 {
     Rigidbody myRigidBody;
     [SerializeField] float flySpeed = 15f;
     [SerializeField] float rotateSpeed;
+    [SerializeField] ParticleSystem dustFX;
+    [SerializeField] GameObject explodeFX;
+    bool isAlive = true;
+    int currentLevel;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody>();
-        
+        currentLevel = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
     void Update()
     {
-        fly();
+        if (isAlive)
+        {
+            fly();
 
-        Rotate();
+            Rotate();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.tag == "bad") ;
+        {
+            Instantiate(explodeFX, transform.position, transform.rotation);
+            
+            isAlive = false;
+            
+
+            Invoke("ResetScene", 2f);
+        }
+        if (collision.gameObject.tag == "win")
+        {
+            SceneManager.LoadScene(currentLevel * 1);
+        }
+
     }
 
     void fly()
@@ -33,7 +54,13 @@ public class ButterflyPlayer : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             myRigidBody.AddRelativeForce(Vector3.up * flySpeed);
+            dustFX.Play();
         }
+        else
+        {
+            dustFX.Stop();
+        }
+    
     }
 
     private void Rotate()
@@ -42,5 +69,10 @@ public class ButterflyPlayer : MonoBehaviour
         float inputAxis = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.back * inputAxis * rotateSpeed);
         myRigidBody.freezeRotation = false;
+    }
+
+    private void ResetScene()
+    {
+        SceneManager.LoadScene(currentLevel);
     }
 }
